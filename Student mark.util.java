43579e58dc504f2e6eaf29e6aeca2b7acf5856f9
ocodnondk <161329926+ocodnondk@users.noreply.github.com>
2024-05-04 01:23:41 +0800

    Update Student mark.util.java

--- Student mark.util.java
+++ Student mark.util.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GradeCalculator {
+public class Studentmarkmanagement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of subjects:");
